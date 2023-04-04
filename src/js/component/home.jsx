bd57e8d2319b6412e01850dbba3ec90747de1376
MGcode23 <marcrudo23@gmail.com>
2023-04-04 15:34:10 +0000

    sin el error

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,85 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+const Tasks = {
+  pending: "",
+};
 
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  const [task, setTask] = useState(Tasks);
+  const [allTask, setAllTask] = useState([]);
+  const [error, setError] = useState(false);
+
+  const handleChange = (event) => {
+    setTask({
+      ...task,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (task.pending.trim() != "") {
+      setAllTask([...allTask, task]);
+      setTask(Tasks);
+      setError(false);
+    } else {
+      setError(true);
+    }
+  };
+
+  const handleAdd = (event) => {
+    event.preventDefault();
+    handleSubmit(event);
+  };
+
+  return (
+    <>
+      <div className="container">
+        <h1>To Do List!</h1>
+        <form onSubmit={handleSubmit} className="d-flex">
+          <input
+            type="text"
+            placeholder="Task"
+            name="pending"
+            className="form-control me-2"
+            value={task.pending}
+            onChange={handleChange}
+          />
+          <button type="submit" className="btn btn-primary">
+            Add
+          </button>
+        </form>
+        {error && (
+          <div className="alert alert-danger">
+            The cell must not be empty <i className="far fa-frown"></i>
+          </div>
+        )}
+        <ul className="list-group">
+          {allTask.map((item, index) => (
+            <li
+              key={index}
+              className="list-group-item d-flex justify-content-between"
+            >
+              {item.pending}
+              <i
+                className="fas fa-trash-alt"
+                onClick={() =>
+                  setAllTask(
+                    allTask.filter(
+                      (t, currentIndex) => index !== currentIndex
+                    )
+                  )
+                }
+              ></i>
+            </li>
+          ))}
+        </ul>
+        <div className="mt-3">
+          {allTask.length} {allTask.length === 1 ? "task" : "tasks"} left
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Home;
